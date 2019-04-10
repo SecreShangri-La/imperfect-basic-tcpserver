150a036b2aeeabff39c3dbfc57cceb8adbe5e69c
ThinkPadX280 <ThinkPadX280@localhost.localdomain>
2019-04-10 18:50:55 +0900

    修正

--- BasicTcpServer.rb
+++ BasicTcpServer.rb
@@ -6,8 +6,7 @@ server = TCPServer.new("0.0.0.0", 1234);
 
 client = server.accept();
 print("[*]Client connects to server\n");
-#line = client.gets().to_s();
-line = gets().to_s();
+line = client.gets().to_s();
 
 if (line == "731\n");
   p ("That's Right");
